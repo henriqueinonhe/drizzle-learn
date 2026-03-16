be9be6ea39331094122d90c0742732d36d520d8d
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2026-03-16 10:47:52 -0300

    Table created

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "drizzle-kit";
+import { env } from "./src/config/env.js";
+export default defineConfig({
+  out: "./database/drizzle",
+  schema: "./src/infrastructure/database/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    host: env.DATABASE_HOST,
+    port: parseInt(env.DATABASE_PORT),
+    database: env.DATABASE_NAME,
+    user: env.DATABASE_USER,
+    password: env.DATABASE_PASSWORD,
+    ssl: false,
+  },
+});
