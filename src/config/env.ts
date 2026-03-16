a0c06b8d66178fde0eca121ce945ad10bcfb4f36
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2026-03-16 10:33:50 -0300

    Setup env

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,9 +4,9 @@ import { cleanEnv, str } from "envalid";
 config();
 
 export const env = cleanEnv(process.env, {
-  ENVIRONMENT: str({
-    choices: ["Development", "Staging", "Production"],
-  }),
-  FIRST_ENV: str(),
-  SECOND_ENV: str(),
+  DATABASE_HOST: str(),
+  DATABASE_PORT: str(),
+  DATABASE_NAME: str(),
+  DATABASE_USER: str(),
+  DATABASE_PASSWORD: str(),
 });
