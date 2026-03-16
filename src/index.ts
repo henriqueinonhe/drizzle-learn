77aa56f08d8b70d148877db3f4825b05a74a99da
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2026-03-16 10:38:21 -0300

    Sample connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { env } from "./config/env.js";
+
 const main = async () => {
-  console.log("HERE");
+  const db = drizzle({
+    connection: {
+      host: env.DATABASE_HOST,
+      port: parseInt(env.DATABASE_PORT),
+      database: env.DATABASE_NAME,
+      user: env.DATABASE_USER,
+      password: env.DATABASE_PASSWORD,
+    },
+  });
+
+  const result = await db.execute("SELECT 1");
+
+  console.log(result);
 };
 
 main();
